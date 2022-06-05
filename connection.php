687b89bda3622fbcc2a3a4a2252347339b321c60
Sarah H <sarahpontoh02@gmail.com>
2022-06-05 08:51:34 +0700

    commit

--- connection.php
+++ connection.php
@@ -1,10 +1,9 @@
 <?php
-    
-    //Database connection using mysqli
-    //$conn = new mysqli('localhost', 'root', '', 'mission11');
-    $conn = mysqli_connect('localhost', 'root', '', 'mission11');
-    if($conn->connect_error)
-    {
-        echo "$conn->connect_error";
-        die('Connection Failed: '.$conn->connect_error);
-    }
+
+//Database connection using mysqli
+//$conn = new mysqli('localhost', 'root', '', 'mission11');
+$conn = mysqli_connect('localhost', 'root', '', 'eshop');
+if ($conn->connect_error) {
+    echo "$conn->connect_error";
+    die('Connection Failed: ' . $conn->connect_error);
+}

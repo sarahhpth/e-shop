0212f2b8d3809d89775536f677de6152e7d33ac2
Sarah H <sarahpontoh02@gmail.com>
2022-06-13 22:28:09 +0700

    vhjvkvul

--- transfer.js
+++ transfer.js
@@ -12,7 +12,7 @@ myHeaders.append("Content-Type", "application/json");
 
 const email = document.querySelector("#email"); //receiver's
 const balance = document.querySelector("#nominal");
-const emoney = document.querySelector("#nominal");
+// const emoney = document.querySelector("#moneygo"); //radio button
 const buttonSubmit = document.querySelector("#submit");
 
 buttonSubmit.addEventListener("click", (e) => {
@@ -45,21 +45,17 @@ buttonSubmit.addEventListener("click", (e) => {
         //response masih dalam bentuk string
         let data_moneygo = await getResponse(moneygo_login);
         // let data_coinless = await getResponse(coinless_login);
-        let data_harpay = await getResponse(harpay_login);
-        let data_ecia = await getResponse(ecia_login);
+        // let data_harpay = await getResponse(harpay_login);
+        // let data_ecia = await getResponse(ecia_login);
         
         //response string dijadiin json
         var resp_moneygo = JSON.parse(data_moneygo);
         // var resp_coinless= JSON.parse(data_coinless);
-        var resp_harpay= JSON.parse(data_harpay);
-        var resp_ecia= JSON.parse(data_ecia);
+        // var resp_harpay= JSON.parse(data_harpay);
+        // var resp_ecia= JSON.parse(data_ecia);
 
         //kalo success
-        if(resp_moneygo.success == true && resp_ecia.status == 200 && resp_harpay.message == "Auth success"){
-            window.localStorage.setItem('moneygo', resp_moneygo.token);
-            //window.localStorage.setItem('coinless', resp_coinless.jwt);
-            window.localStorage.setItem('harpay', resp_harpay.token);
-            window.localStorage.setItem('ecia', resp_ecia.token);
+        if(resp_moneygo.status == 200){
             
             window.location.href = "index2.php";
         }else{

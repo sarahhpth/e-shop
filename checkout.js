58f2cdefb1e8cec27fb14af922d351a8b555f6a1
Sarah H <sarahpontoh02@gmail.com>
2022-06-14 21:22:47 +0700

    jyhtgfdbgbg

--- checkout.js
+++ checkout.js
@@ -1,22 +1,15 @@
-function parseJwt (token) {
-    var base64 = token.split('.')[1];
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-  
-    return JSON.stringify(jsonPayload);
-};
-  
-// var parsetoken = require('./parseJWT');
-
 //ambil token dari local.Storage. local storage ada di page source >> application
 const moneygo = JSON.stringify(localStorage.getItem('moneygo'));
-const ecia = JSON.stringify(localStorage.getItem('ecia'));
-const harpay = JSON.stringify(localStorage.getItem('harpay'));
-const coinless = JSON.stringify(localStorage.getItem('coinless'));
+// const ecia = JSON.stringify(localStorage.getItem('ecia'));
+// const harpay = JSON.stringify(localStorage.getItem('harpay'));
+// const coinless = JSON.stringify(localStorage.getItem('coinless'));
 
-const balance = document.querySelector("#nominal");
+
+// const hp = document.querySelector("#hp"); //hp penerima harpay
+// const email = document.querySelector("#email"); //receiver's
+const price = document.getElementById("#price");
 const emoney = document.querySelector("#emoney"); 
+// const pin = document.querySelector("#pin"); //pin harpay
 const buttonSubmit = document.querySelector("#submit");
 
 buttonSubmit.addEventListener("click", (e) => {
@@ -25,36 +18,42 @@ buttonSubmit.addEventListener("click", (e) => {
     var selected = emoney.options[emoney.selectedIndex].value; //selected value from the drop down
   
     if(selected == "Moneygo"){
-        var api_topup = "https://moneygo-api.herokuapp.com/api/topup";
+        var api_transaksi = "https://moneygo-api.herokuapp.com/api/transaksi";
         var token = ("Bearer " + moneygo).replace(/\"/g, ""); //variable untuk nyimpen token. ini yg dikirim ke api
-        var method = "PUT";
+        var method = "POST";
+
+        var raw = JSON.stringify({
+            balance: price.value
+        });
 
     }
     if(selected == "Coinless"){
-        var dataToken = parseJwt(coinless);
-        var id = dataToken.id_user;
-
-        var api_topup = "https://coinless.herokuapp.com/api/profile/" + id;
+        var api_transaksi = "https://coinless.herokuapp.com/api/transfer";
         var token = ("Bearer " + coinless).replace(/\"/g, ""); //variable untuk nyimpen token. ini yg dikirim ke api
-        var method = "PUT";
+        var method = "POST";
+
+        var raw = JSON.stringify({
+            email: email.value,
+            balance: balance.value
+        });
         
     }
     if(selected == "Harpay"){
-        var api_topup = "https://harpay-api.herokuapp.com/transaksi/topup";
+        var api_transaksi = " https://harpay-api.herokuapp.com/transaksi/transferSaldo";
         var token = ("Bearer " + harpay).replace(/\"/g, ""); //variable untuk nyimpen token. ini yg dikirim ke api
         var method = "POST";
-        
+
+        var raw = JSON.stringify({
+        noTelp: hp.value,
+        nominal: balance.value,
+        pin: pin.value
+    });
     }
 
     var myHeaders = new Headers();
     myHeaders.append("Authorization", token);
     myHeaders.append("Content-Type", "application/json");
-
-    var raw = JSON.stringify({
-        balance: balance.value,
-        nominal: balance.value,
-        jumlah: balance.value
-    });
+    
 
     var requestOptions = {
         method: method,
@@ -65,7 +64,7 @@ buttonSubmit.addEventListener("click", (e) => {
 
     async function getResponse(){
         try {
-            let res = await fetch(api_topup, requestOptions)
+            let res = await fetch(api_transaksi, requestOptions)
             console.log("Fetch berhasil");
             return await (res.text());
         } catch(error) {
@@ -89,8 +88,8 @@ buttonSubmit.addEventListener("click", (e) => {
         // var resp_ecia= JSON.parse(data_ecia);
 
         //kalo success
-        if(resp_api.status == 200 || resp_api.message == "Topup successfully!"){
-            
+        if(resp_api.status == 200){
+            alert(resp_api.message);
             window.location.href = "home.php";
         }else{
             alert(resp_api.message);
